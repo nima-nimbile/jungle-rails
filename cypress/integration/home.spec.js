6772bdff3d43a6575610530314c420f90377b950
Nima <nima.mohamed@hotmail.com>
2023-04-12 15:23:11 -0700

    add cypress test for homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+describe('Jungle home page', () => {
+
+ 
+  it("can visit the home page", () => {
+    cy.visit('http://0.0.0.0:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
