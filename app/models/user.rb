456703973f08eb9d0540a33af05c3d7f0928f004
Nima <nima.mohamed@hotmail.com>
2023-04-13 18:54:32 -0700

    Fix some buges

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true , uniqueness:{ case_sensitive:false }
-  validates :password, presence: true, length:{ minimum:6 }
+  validates :password, presence: true, length:{ minimum:3 }
   
   def self.authenticate_with_credentials (email, password)
     @no_spaces_email =email.strip
